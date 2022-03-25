47607733bf67d44fddd1ff36d4fe34c9ec1c2360
sapirdeu <sapiroosh.d@gmail.com>
2022-03-25 13:47:53 +0300

    new form

---
+++ redditclone/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'redditclone';
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(title.value, link.value)
+    return false;
+  }
+}
